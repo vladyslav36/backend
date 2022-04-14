38410339fea8d3f5b5bfd16c9fa9539de8832fb6
Vladyslav <vladyslav36@gmail.com>
2022-04-14 12:41:13 +0300

    14042022

--- index.js
+++ index.js
@@ -1,28 +1,25 @@
-const path =require('path')
-const express = require('express')
-const cors=require('cors')
-const dotenv=  require ('dotenv')
-const connectDb =require ('./config/db.js')
-const morgan= require ('morgan')
-const { notFound } = require ('./middleware/notFound.js')
-const { errorHandler } = require('./middleware/errorHandler.js')
-const productsRouter = require('./routes/products')
-const categoriesRouter = require('./routes/categories')
-const currencyRateRouter = require('./routes/currencyRate')
-const uploadRouter = require('./routes/upload')
-const cartRouter = require('./routes/cart')
-const userRouter = require('./routes/user')
-const searchRouter = require('./routes/search')
+const https = require("https")
+const fs = require("fs")
+const path = require("path")
+const express = require("express")
+const cors = require("cors")
+const dotenv = require("dotenv")
+const connectDb = require("./config/db.js")
+const morgan = require("morgan")
+const { notFound } = require("./middleware/notFound.js")
+const { errorHandler } = require("./middleware/errorHandler.js")
+const productsRouter = require("./routes/products")
+const categoriesRouter = require("./routes/categories")
+const currencyRateRouter = require("./routes/currencyRate")
+const uploadRouter = require("./routes/upload")
+const cartRouter = require("./routes/cart")
+const userRouter = require("./routes/user")
+const searchRouter = require("./routes/search")
 // const optionsRouter=require('./routes/options')
-const orderRouter = require('./routes/order')
-const informationRouter = require('./routes/information')
+const orderRouter = require("./routes/order")
+const informationRouter = require("./routes/information")
 
-
-
-
-
-
-dotenv.config({ path: './config/.env' })
+dotenv.config({ path: "./config/.env" })
 process.env.ROOT_NAME = path.dirname(__filename)
 
 connectDb()
@@ -30,26 +27,30 @@ connectDb()
 const app = express()
 
 
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev')) 
-}
-app.use(cors())  
-app.use(express.json())
-app.use('/upload',express.static(path.join(__dirname,'/upload')))
-app.use('/api/user',userRouter)
-app.use('/api/products', productsRouter)
-app.use('/api/categories', categoriesRouter)
-app.use('/api/cart',cartRouter)
-app.use('/api/currencyrate', currencyRateRouter)
-app.use('/api/upload', uploadRouter)
-app.use('/api/search', searchRouter)
-app.use('/api/order', orderRouter)
-app.use('/api/information',informationRouter)
 
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"))
+}
+app.use(cors())
+app.use(express.json())
+app.use("/upload", express.static(path.join(__dirname, "/upload")))
+app.use("/api/user", userRouter)
+app.use("/api/products", productsRouter)
+app.use("/api/categories", categoriesRouter)
+app.use("/api/cart", cartRouter)
+app.use("/api/currencyrate", currencyRateRouter)
+app.use("/api/upload", uploadRouter)
+app.use("/api/search", searchRouter)
+app.use("/api/order", orderRouter)
+app.use("/api/information", informationRouter)
 
 app.use(notFound)
 app.use(errorHandler)
 
-const PORT=process.env.PORT || 5000
-app.listen(PORT, console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+const PORT = process.env.PORT || 5000
+app.listen(
+  PORT,
+  console.log(
+    `Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`
+  )
 )
