12ee6b1f858128893ee33b0adc93691dea616cc3
Vladyslav <vladyslav36@gmail.com>
2022-04-23 08:18:16 +0300

    23042022

--- controllers/user.js
+++ controllers/user.js
@@ -28,9 +28,9 @@ exports.registerUser = asyncHandler(async (req, res) => {
     throw new Error('User already exists')
   } 
     const user = await User.create({
-    name,
-    email,
-    password
+    name:name.trim(),
+    email:email.trim(),
+    password:password.trim()
   })
   
   if (user) {
