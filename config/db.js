56874fa8d3c1b6efca41c9338f7e97d3c153f12e
Vladyslav <vladyslav36@gmail.com>
2022-05-01 14:06:16 +0300

    update dep

--- config/db.js
+++ config/db.js
@@ -2,11 +2,7 @@ const mongoose = require("mongoose")
 
 const connectDb = async (req, res) => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-    })
+    const conn = await mongoose.connect(process.env.MONGO_URI)
     console.log(`MongoDb connect ${conn.connection.host}`)
   } catch (err) {
     console.log(`Error ${err.message}`)
--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-const https = require("https")
-const fs = require("fs")
+
 const path = require("path")
 const express = require("express")
 const cors = require("cors")
