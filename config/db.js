d0d1954f879455003565af3d132f006a1e80e862
vladyslav36 <vladyslav36@ukr.net>
2023-04-04 20:22:49 +0300

    04042023

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose")
 const connectDb = async (req, res) => {
   try {
     mongoose.set( 'strictQuery', false)
-    const conn = await mongoose.connect(process.env.MONGO_URI_TEST)
+    const conn = await mongoose.connect(process.env.MONGO_URI)
     console.log(`MongoDb connect ${conn.connection.host}`)
   } catch (err) {
     console.log(`Error ${err.message}`)
