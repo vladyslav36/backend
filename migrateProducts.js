984daebd40aae59fff3d8bb99aae8e4984845023
vladyslav36 <vladyslav36@ukr.net>
2023-04-05 11:50:01 +0300

    05042023

--- migrateProducts.js
+++ migrateProducts.js
@@ -11,26 +11,39 @@ const migrateProducts = async () => {
   await Promise.all(
     products.map(async (product) => {
       const { options } = product
-      const newOptions = Object.assign(
-        {},
-        ...Object.keys(options).map((option) => {
-          return {
-            [option]: Object.assign({}, ...Object.keys(options[option].values)
-              .filter((item) => options[option].values[item].checked)
-              .map((value) => ({
-                [value]: {
-                price: options[option].values[value].price
-                  ? options[option].values[value].price
-                    : product.price,
-                  isChanged: options[option].values[value].price ? true : false,
-                  barcode:''
-              }
+      // const newOptions = Object.assign(
+      //   {},
+      //   ...Object.keys(options).map((option) => {
+      //     return {
+      //       [option]: Object.assign({}, ...Object.keys(options[option].values)
+      //         .filter((item) => options[option].values[item].checked)
+      //         .map((value) => ({
+      //           [value]: {
+      //           price: options[option].values[value].price
+      //             ? options[option].values[value].price
+      //               : product.price,
+      //             isChanged: options[option].values[value].price ? true : false,
+      //             barcode:''
+      //         }
                
-              }))) 
-          }
-        })
-      )
-
+      //         }))) 
+      //     }
+      //   })
+      // )
+      // --------------------------------------------------------------------------------
+      const newOptions = Object.assign({}, ...Object.keys(options).map(option => {        
+        return {
+          [option]: Object.assign({}, ...Object.keys(options[option]).map(value => {
+            const { barcode, ...newValue }=options[option][value]
+            return {
+              [value]:newValue
+            }     
+            
+          }))
+         
+        }
+      }))
+// ---------------------------------------------------------------------------------------
       await Product.findOneAndUpdate({ _id: product._id },{options:newOptions})
 
     })
