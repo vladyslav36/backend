a6cdea319ebf67e9e99d39866b5701ba500ed0d9
vladyslav36 <vladyslav36@ukr.net>
2023-04-15 08:28:22 +0300

    15042023

--- test.js
+++ test.js
@@ -46,6 +46,67 @@ const optionsToBarcods = (input = {}) => {
 
   return output
 }
-const output = optionsToBarcods({})
-  console.log(output)
+// const output = optionsToBarcods(input)
+  
+const barcods = {
+  2: {
+    синий: { 96: "a", 102: "", 108: "", 112: "" },
+    красный: { 96: "", 102: "s", 108: "", 112: "" },
+  },
+  3: {
+    синий: { 96: "", 102: "d", 108: "", 112: "f" },
+    красный: { 96: "", 102: "g", 108: "", 112: "" },
+  },
+  4: {
+    синий: { 96: "", 102: "", 108: "", 112: "h" },
+    красный: { 96: "", 102: "j", 108: "", 112: "" },
+  },
+}
+
+const newBarcods = {
+  2: {
+    синий: { 96: "", 102: "", 112: "" },
+    красный: { 96: "", 102: "", 112: "" },
+    желтый: { 96: "", 102: "", 112: "" },
+  },
+  3: {
+    синий: { 96: "", 102: "", 112: "" },
+    красный: { 96: "", 102: "", 112: "" },
+    желтый: { 96: "", 102: "", 112: "" },
+  },
+  4: {
+    синий: { 96: "", 102: "", 112: "" },
+    красный: { 96: "", 102: "", 112: "" },
+    желтый: { 96: "", 102: "", 112: "" },
+  },
+}
+
+// const crumbsArr = [2, 'синий', 96]
+
+// const lastObj = crumbsArr.slice(0, -1).reduce((acc, item) => acc[item], barcods)
+// console.log(lastObj)
+// const lastKey = crumbsArr[crumbsArr.length - 1]
+// console.log(lastKey)
+// lastObj[lastKey] = '100500'
+// console.log(barcods)
+
+
+const copyBarcods = (existBc, newBc) => {
+  const checker = (existObj,newObj) => Object.keys(newObj).forEach(item => {
+   
+    if (!existObj.hasOwnProperty(item)) return
+    if (typeof newObj[item] === "object") {
+      checker(existObj[item],newObj[item])
+    } else {
+      newObj[item] = existObj[item]
+    }
+      
+    
+  })
+  
+  checker(existBc, newBc)
+  return newBc
+}
+
+console.log(copyBarcods(barcods,newBarcods)) 
 
